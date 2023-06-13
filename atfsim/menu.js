f1c3021e6adb1b6e19258a5560f4e3716b310c72
Renblas <renblas.dev@gmail.com>
2023-06-12 22:42:40 -0400

    buttons work

--- atfsim/menu.js
+++ atfsim/menu.js
@@ -8,12 +8,19 @@
 /* ----------------------------- Button Handlers ---------------------------- */
 
 // start button
-document.getElementById("start-button").children[0].addEventListener("click", function() {
-	print("I was clicked!")
-	document.getElementsByClassName("menu-main")[0].setAttribute("style", "visibility: hidden")
-})
+document.getElementById("start-button").children[0].addEventListener("click", function () {
+	print("START-BUTTON was clicked!");
+	document.getElementById("menu-main").setAttribute("style", "visibility: hidden");
+});
 
 // settings button
-document.getElementById("settings-button").addEventListener("click", function() {
-	print("I was clicked!")
-})
+document.getElementById("settings-button").addEventListener("click", function () {
+	print("SETTINGS-BUTTON was clicked!");
+	if (menuSettingsEnabled == true) {
+		document.getElementById("menu-settings").setAttribute("style", "visibility: hidden")
+		menuSettingsEnabled = false;
+	} else {
+		document.getElementById("menu-settings").setAttribute("style", "visibility: visible")
+		menuSettingsEnabled = true;
+	}
+});
