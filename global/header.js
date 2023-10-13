6ec992d1eb43b8dafe46d55d126d8ae763baed00
Renblas <renblas.dev@gmail.com>
2023-10-13 11:37:49 -0400

    portfolio and files

--- global/header.js
+++ global/header.js
@@ -1,23 +1,25 @@
 
 // Define Global Header Class
 class HTML_Header extends HTMLElement {
-    constructor() {
-        super();
-    }
+	constructor() {
+		super();
+	}
 
-    connectedCallback() {
-        this.innerHTML = `
+	connectedCallback() {
+		this.innerHTML = `
         <header class="header"> 
 			<nav class="header">
             	<ul class="header">
                		<li class="header"><a class="header" href="index.html">Home</a></li>
                		<li class="header"><a class="header" href="contact.html">Contact</a></li>
+               		<li class="header"><a class="header" href="portfolio.html">Portfolio</a></li>
+               		<li class="header"><a class="header" href="files">Files</a></li>
                		<li class="header" style="float:right"><a class="header" href="games.html">Games</a></li>
             	</ul>
 			</nav>
         </header>
         `;
-    }
+	}
 }
 
 // Register Class to HTML
